e4d6130d98e8b797f6596cd41d1fd8e687e8ce9a
Berfin <101349315+BerfinUn@users.noreply.github.com>
2024-09-29 18:01:51 +0300

    Düzenlemeler yapildi

--- Foody.PresentationLayer/Program.cs
+++ Foody.PresentationLayer/Program.cs
@@ -1,6 +1,18 @@
+using Foody.BusinessLayer.Abstract;
+using Foody.BusinessLayer.Concrete;
+using Foody.DataAccessLayer.Abstract;
+using Foody.DataAccessLayer.Context;
+using Foody.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<FoodyContext>();
+
+builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
