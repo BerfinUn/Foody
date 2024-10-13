3597c1745484e4347fbeaa67452f19dada16a514
Berfin <101349315+BerfinUn@users.noreply.github.com>
2024-10-13 18:04:17 +0300

    add default pages

---
+++ Foody.PresentationLayer/ViewComponents/DefaultViewComponents/_DefaultHeadSectionComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresentationLayer.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultHeadSectionComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
