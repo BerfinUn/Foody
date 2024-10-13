7d4f19537d2234584a45c4cb8e4fd4bbad00c9bd
Berfin <101349315+BerfinUn@users.noreply.github.com>
2024-10-13 19:23:14 +0300

    slider section view

---
+++ Foody.PresentationLayer/ViewComponents/DefaultViewComponents/_DefaultNavbarSectionComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresentationLayer.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultNavbarSectionComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
