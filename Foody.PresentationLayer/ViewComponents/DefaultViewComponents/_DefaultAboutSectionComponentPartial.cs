f661a6ef06b50cef7ba3e108af9c7681ade42b23
Berfin <101349315+BerfinUn@users.noreply.github.com>
2024-10-13 19:43:11 +0300

    about pages default

---
+++ Foody.PresentationLayer/ViewComponents/DefaultViewComponents/_DefaultAboutSectionComponentPartial.cs
@@ -0,0 +1,21 @@
+﻿using Foody.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresentationLayer.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultAboutSectionComponentPartial:ViewComponent
+    {
+        private readonly IAboutService _aboutService;
+
+        public _DefaultAboutSectionComponentPartial(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _aboutService.TGetAll();
+            return View(values);
+        }
+    }
+}
